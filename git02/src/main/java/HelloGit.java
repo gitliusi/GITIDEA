5ae214285012c2583800653d161ded8c9ad38481
gitliusi <875174758@qq.com>
2022-07-07 13:01:59 +0800

    创建git02

---
+++ git02/src/main/java/HelloGit.java
@@ -0,0 +1,5 @@
+public class HelloGit {
+    public void display(){
+        System.out.println("display git");
+    }
+}
---
+++ git02/src/test/java/HelloGitTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Test;
+
+public class HelloGitTest {
+    @Test
+    public void displayTest(){
+        HelloGit h = new HelloGit();
+        h.display();
+    }
+}
